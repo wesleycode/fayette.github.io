b712b07f66e94ff0942fc4bbd544d392c0b5daa1
wesleycode <wesleynuke76@gmail.com>
2025-06-17 13:32:07 -0300

    atualizando projeto

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export function App() {
+	return (
+		<div className="w-full bg-[url(/background.png)] bg-auto bg-no-repeat bg-amber-700 h-screen flex items-center justify-center">
+			<div className="flex flex-col justify-center items-center text-center min-h-54 gap-3 bg-gray-300/50 p-5 rounded-2xl max-w-1/4">
+				<h1 className="text-3xl text-gray-700 font-bold uppercase">
+					Em Breve
+				</h1>
+				<p className="text-gray-500 font-medium">Estamos trabalhando duro para trazer algo incrível!</p>
+			</div>
+		</div>
+	)
+}
